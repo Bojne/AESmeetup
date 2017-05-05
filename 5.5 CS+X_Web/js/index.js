4e38a84f6e9e37d479080990bd88d4cfd532270e
Bojne <bojne.john@gmail.com>
2017-05-05 20:50:16 +0800

    Add files via upload

--- 5.5 CS+X_Web/js/index.js
+++ 5.5 CS+X_Web/js/index.js
@@ -1,9 +1,13 @@
-var fnChange = function() {
-    //取亂數 1~10
-}
-
+//產生1~n的亂數
 var rand = function(n) {
     var rlt = Math.floor(Math.random() * n) + 1;
 }
+
+
+var fnChange = function() {
+    var index = rand(10);
+    $('#ection2> div.image > img').attr('src', './imgae/0' + index + '.jpg')
+}
+
 
 $('#change').on('click', fnChange);
